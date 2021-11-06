5406bc404439c616fbc74a1063d32e0a1b08890c
EqualMa <equalma@outlook.com>
2021-11-06 22:44:52 +0800

    chore: setup semantic-release

--- release.config.js
+++ release.config.js
@@ -1,3 +1,10 @@
+const CARGO_PROJECTS = [
+  //
+  "frender",
+];
+
+const CARGO_FILES = CARGO_PROJECTS.map((dir) => `${dir}/Cargo.toml`);
+
 module.exports = {
   branches: [
     "+([0-9])?(.{+([0-9]),x}).x",
@@ -11,9 +18,34 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    ["@semantic-release/npm", { npmPublish: false }],
-    ["@semantic-release/npm", { pkgRoot: "dist" }],
-    "@semantic-release/git",
+    [
+      "@google/semantic-release-replace-plugin",
+      {
+        replacements: [
+          {
+            files: CARGO_FILES,
+            from: 'version = ".*" # replace version',
+            to: 'version = "${nextRelease.version}" # replace version',
+            results: CARGO_FILES.map((file) => ({
+              file,
+              hasChanged: true,
+              numMatches: 1,
+              numReplacements: 1,
+            })),
+            countMatches: true,
+          },
+        ],
+      },
+    ],
+    ["@semantic-release/exec", { prepareCmd: "cargo check" }],
+    ...CARGO_PROJECTS.map((pro) => [
+      "@semantic-release/exec",
+      { publishCmd: "cargo publish", execCwd: pro },
+    ]),
+    [
+      "@semantic-release/git",
+      { assets: ["CHANGELOG.md", "Cargo.lock", ...CARGO_FILES] },
+    ],
     "@semantic-release/github",
   ],
 };
