6668e9bfaa394d44415bf1f9ee09d2787a398dc6
EqualMa <equalma@outlook.com>
2022-02-28 20:06:15 +0800

    chore: publish pages in ci

---
+++ scripts/build-examples.js
@@ -0,0 +1,130 @@
+const { spawn } = require("child_process");
+const fsp = require("fs/promises");
+const path = require("path");
+
+function wrapHtml(title, content) {
+  return `\
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>${title}</title>
+  </head>
+  <body>
+    <div style="max-width: 768px; margin: 0 auto; padding: 16px;">${content}</div>
+  </body>
+</html>
+`;
+}
+
+function indexPageHtml(examplesListHtml) {
+  return wrapHtml(
+    "Frender examples",
+    `\
+<div style="max-width: 768px; margin: auto; text-align: center">
+<img src="/logo.svg" alt="frender logo" style="max-width: 100%; max-height: 240px">
+<p>
+<a href="https://crates.io/crates/frender" rel="nofollow"><img src="https://img.shields.io/crates/v/frender?style=for-the-badge" alt="Crates.io"></a>
+<a href="https://docs.rs/frender" rel="nofollow"><img src="https://img.shields.io/docsrs/frender/latest?style=for-the-badge" alt="docs.rs"></a>
+<a href="https://github.com/frender-rs/frender/blob/main/LICENSE" rel="nofollow"><img src="https://img.shields.io/github/license/frender-rs/frender?style=for-the-badge" alt="GitHub license"></a>
+<a href="https://github.com/frender-rs/frender/stargazers" rel="nofollow"><img src="https://img.shields.io/github/stars/frender-rs/frender?style=for-the-badge&logo=github" alt="GitHub stars"></a>
+</p>
+<p>Functional Rendering: <code>React</code> in <code>Rust</code></p>
+</div>
+<div style="margin: 32px 0;">
+<h2>Examples</h2>
+${examplesListHtml}
+</div>
+<div>
+<script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support frender on Ko-fi', '#ff8ad8', 'N4N26J11L');kofiwidget2.draw();</script>
+</div>
+`,
+  );
+}
+
+function examplesPageHtml(examplesListHtml) {
+  return wrapHtml(
+    "Frender examples",
+    `\
+<h1><i>frender</i> examples</h1>
+${examplesListHtml}
+`,
+  );
+}
+
+function getExamplesListHtml(examples) {
+  return `\
+<ul>
+${examples.map(({ name }) => {
+  const nameEncoded = encodeURIComponent(name);
+  return `<li><a target="_blank" href="/examples/${nameEncoded}">${name}</a></li>`;
+})}
+</ul>
+`;
+}
+
+async function listExamples(examplesFolder) {
+  let dirents = await fsp.readdir(examplesFolder, { withFileTypes: true });
+
+  return dirents
+    .filter((ent) => ent.isDirectory())
+    .map((ent) => {
+      const folder = path.join(examplesFolder, ent.name);
+      const indexFile = path.join(folder, "index.html");
+      return {
+        name: ent.name,
+        folder,
+        indexFile,
+      };
+    });
+}
+
+async function main() {
+  const DIST_DIR = "dist";
+  const DIST_EXAMPLES_DIR = "dist/examples";
+  await fsp.mkdir(DIST_EXAMPLES_DIR, { recursive: true });
+  const examples = await listExamples("examples");
+  for (const { name, indexFile } of examples) {
+    const child = spawn("trunk", [
+      "build",
+      "-d",
+      `dist/examples/${name}`,
+      "--public-url",
+      `examples/${name}`,
+      indexFile,
+    ]);
+
+    const code = await new Promise((resolve) => {
+      child.on("exit", (code) => {
+        resolve(code);
+      });
+    });
+
+    if (code !== 0) {
+      throw new Error(
+        `Example ${name} failed to build. Exited with status ${code}`,
+      );
+    } else {
+      console.log(`[Example built success] ${name}`);
+    }
+  }
+
+  const examplesListHtml = getExamplesListHtml(examples);
+
+  await Promise.all([
+    fsp.writeFile(
+      path.join(DIST_DIR, "index.html"),
+      indexPageHtml(examplesListHtml),
+    ),
+    fsp.writeFile(
+      path.join(DIST_EXAMPLES_DIR, "index.html"),
+      examplesPageHtml(examplesListHtml),
+    ),
+    fsp.copyFile("logo.svg", path.join(DIST_DIR, "logo.svg")),
+  ]);
+}
+
+main().catch((err) => {
+  process.exitCode = 1;
+  console.error(err);
+});
