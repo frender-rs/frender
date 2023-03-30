1b3ae1ac7b0742cbecb80eb72e219e02d131da57
Equal Ma <equalma@outlook.com>
2023-03-30 17:11:51 +0800

    feat: now `Option<RenderState>` and `Option<Element>` allows State to be `!Unpin`

--- crates/frender-csr/src/elements/option.rs
+++ crates/frender-csr/src/elements/option.rs
@@ -2,16 +2,16 @@ use std::pin::Pin;
 
 use crate::{Element, RenderState};
 
-impl<S: RenderState + Unpin> RenderState for Option<S> {
-    fn unmount(self: Pin<&mut Self>) {
-        let this = self.get_mut();
+impl<S: RenderState> RenderState for Option<S> {
+    fn unmount(mut self: Pin<&mut Self>) {
+        let this = self.as_mut().as_pin_mut();
         match this {
             Some(state) => {
-                S::unmount(Pin::new(state));
+                S::unmount(state);
             }
             None => return,
         }
-        *this = None;
+        self.set(None)
     }
 
     fn poll_csr(
@@ -19,17 +19,14 @@ impl<S: RenderState + Unpin> RenderState for Option<S> {
         ctx: &mut crate::CsrContext,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<()> {
-        match self.get_mut() {
-            Some(s) => S::poll_csr(Pin::new(s), ctx, cx),
+        match self.as_pin_mut() {
+            Some(s) => S::poll_csr(s, ctx, cx),
             None => std::task::Poll::Ready(()),
         }
     }
 }
 
-impl<E: Element> Element for Option<E>
-where
-    <E as Element>::CsrState: Unpin,
-{
+impl<E: Element> Element for Option<E> {
     type CsrState = Option<E::CsrState>;
 
     fn into_csr_state(self, ctx: &mut crate::CsrContext) -> Self::CsrState {
@@ -39,11 +36,11 @@ where
         }
     }
 
-    fn update_csr_state(self, ctx: &mut crate::CsrContext, state: Pin<&mut Self::CsrState>) {
+    fn update_csr_state(self, ctx: &mut crate::CsrContext, mut state: Pin<&mut Self::CsrState>) {
         if let Some(this) = self {
-            match state.get_mut() {
-                Some(state) => this.update_csr_state(ctx, Pin::new(state)),
-                state => *state = Some(this.into_csr_state(ctx)),
+            match state.as_mut().as_pin_mut() {
+                Some(state) => this.update_csr_state(ctx, state),
+                None => state.set(Some(this.into_csr_state(ctx))),
             };
         } else {
             <Option<E::CsrState> as RenderState>::unmount(state)
--- crates/frender-ssr/src/element/option.rs
+++ crates/frender-ssr/src/element/option.rs
@@ -2,23 +2,21 @@ use std::pin::Pin;
 
 use crate::{Element, RenderState};
 
-impl<S: RenderState + Unpin> RenderState for Option<S> {
+impl<S: RenderState> RenderState for Option<S> {
     fn poll_render<W: futures_io::AsyncWrite + ?Sized>(
         self: Pin<&mut Self>,
         writer: Pin<&mut W>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<std::io::Result<()>> {
-        match self.get_mut() {
-            Some(s) => S::poll_render(Pin::new(s), writer, cx),
-            None => std::task::Poll::Ready(Ok(())),
+        if let Some(s) = self.as_pin_mut() {
+            S::poll_render(s, writer, cx)
+        } else {
+            std::task::Poll::Ready(Ok(()))
         }
     }
 }
 
-impl<E: Element> Element for Option<E>
-where
-    <E as Element>::SsrState: Unpin,
-{
+impl<E: Element> Element for Option<E> {
     type SsrState = Option<E::SsrState>;
 
     fn into_ssr_state(self) -> Self::SsrState {
