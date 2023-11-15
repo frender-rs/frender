cd221c01064a3511e44b228906d64281e4e70ce9
Equal Ma <equalma@outlook.com>
2023-11-16 03:45:07 +0800

    wip

--- crates/frender-html-components/src/lib.rs
+++ crates/frender-html-components/src/lib.rs
@@ -3,8 +3,12 @@ mod common;
 pub mod html {
 
     frender_html::expand_html_traits!(
-        #[props]
+        #[props_without_builders]
         pub mod props;
+
+        #[props_builders]
+        mod props_builders;
+
         #[components]
         pub mod components;
     );
