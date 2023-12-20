f082f303a589aec4a4a91caa8321469de4a46d0c
Equal Ma <equalma@outlook.com>
2023-12-20 13:18:30 +0800

    feat: intrinsic macro now supports more than 12 elements

--- frender/src/element_macros.rs
+++ frender/src/element_macros.rs
@@ -128,14 +128,29 @@ macro_rules! intrinsic {
             $([[$($children:tt)*]])?
         ),+ $(,)?
     ) => {
-        ($(
+        $crate::__impl_intrinsic_many! {$({
             $crate::__impl_intrinsic_one! {
                 $first
                 $(($($inner)*))?
                 $( . $method $(($($method_arg)*))? )*
                 $(.children($crate::intrinsic![$($children)*]))?
-            },
-        )+)
+            }
+        })+}
+    };
+}
+
+#[macro_export]
+macro_rules! __impl_intrinsic_many {
+    ({ $t0:expr } { $t1:expr } { $t2:expr } { $t3:expr } { $t4:expr } { $t5:expr } { $t6:expr } { $t7:expr } { $t8:expr } { $t9:expr } { $t10:expr } { $t11:expr } $($t:tt)+) => {
+        (
+            (
+                $t0, $t1, $t2, $t3, $t4, $t5, $t6, $t7, $t8, $t9, $t10, $t11,
+            ),
+            $crate::__impl_intrinsic_many! { $($t)+ },
+        )
+    };
+    ($({ $t0:expr })+)=>{
+        ($($t0,)+)
     };
 }
 
