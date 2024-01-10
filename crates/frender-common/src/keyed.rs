8a4f1992e3c3700fcb4d7a1201e080d4b8be9175
Equal Ma <equalma@outlook.com>
2024-01-10 17:03:24 +0800

    chore: remove unused code

--- crates/frender-common/src/keyed.rs
+++ crates/frender-common/src/keyed.rs
@@ -1,23 +1,6 @@
 #[derive(Debug, Clone, Copy)]
 pub struct Keyed<K, E>(pub K, pub E);
 
-mod sealed {
-    pub trait Sealed {}
-
-    impl<K, E> Sealed for super::Keyed<K, E> {}
-}
-
-/// Only [`Keyed<K, E>`] implements [`IsKeyed<Key = K, Element = E>`].
-pub trait IsKeyed: sealed::Sealed {
-    type Key;
-    type Element;
-}
-
-impl<K, E> IsKeyed for Keyed<K, E> {
-    type Key = K;
-    type Element = E;
-}
-
 impl<K, E> Keyed<K, E> {
     pub fn from_tuple((key, element): (K, E)) -> Self {
         Keyed(key, element)
