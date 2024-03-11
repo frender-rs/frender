210e7f3d4b68fcbf769c636dc24f19fe67cf0f05
Equal Ma <equalma@outlook.com>
2024-03-11 17:00:59 +0800

    feat: `ToSsrElement`

--- crates/frender-ssr/src/trait_element.rs
+++ crates/frender-ssr/src/trait_element.rs
@@ -3,3 +3,24 @@ pub trait SsrElement {
 
     fn into_html_children(self) -> Self::HtmlChildren;
 }
+
+pub trait ToSsrElement {
+    type ToSsrElement: SsrElement;
+    fn to_ssr_element(&self) -> Self::ToSsrElement;
+}
+
+impl<E: Copy + SsrElement> ToSsrElement for E {
+    type ToSsrElement = Self;
+
+    fn to_ssr_element(&self) -> Self::ToSsrElement {
+        *self
+    }
+}
+
+impl ToSsrElement for str {
+    type ToSsrElement = String;
+
+    fn to_ssr_element(&self) -> Self::ToSsrElement {
+        self.to_owned()
+    }
+}
