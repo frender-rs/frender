2c4e0595723f880aaa2911e352657b19508c3e4a
Equal Ma <equalma@outlook.com>
2024-03-11 17:03:21 +0800

    feat: trait `RenderAsText`

--- crates/frender-dom/src/render.rs
+++ crates/frender-dom/src/render.rs
@@ -45,3 +45,55 @@ impl<R: ?Sized, Text> RenderTextFromKnown<Text> for R where
 {
 }
 
+pub trait RenderAsText {
+    fn render_as_text<R: ?Sized + RenderTextFromKnown<Text>, Text>(&self, renderer: &mut R)
+        -> Text;
+
+    fn render_as_text_update<R: ?Sized + RenderTextFromKnown<Text>, Text>(
+        &self,
+        renderer: &mut R,
+        text: &mut Text,
+    );
+}
+
+frender_common::impl_many!(
+    impl<__> RenderAsText
+        for each_of![
+            str, //
+            i8, u8, i16, u16, i32, u32, i64, u64, i128, u128, isize, usize, f32, f64, //
+            char,
+        ]
+    {
+        fn render_as_text<R: ?Sized + RenderTextFromKnown<Text>, Text>(
+            &self,
+            renderer: &mut R,
+        ) -> Text {
+            renderer.render_text_from(self)
+        }
+
+        fn render_as_text_update<R: ?Sized + RenderTextFromKnown<Text>, Text>(
+            &self,
+            renderer: &mut R,
+            text: &mut Text,
+        ) {
+            renderer.update_text_from(text, self)
+        }
+    }
+);
+
+impl<S: std::borrow::Borrow<str>> RenderAsText for frender_common::TempStr<S> {
+    fn render_as_text<R: ?Sized + RenderTextFromKnown<Text>, Text>(
+        &self,
+        renderer: &mut R,
+    ) -> Text {
+        str::render_as_text(self.borrow(), renderer)
+    }
+
+    fn render_as_text_update<R: ?Sized + RenderTextFromKnown<Text>, Text>(
+        &self,
+        renderer: &mut R,
+        text: &mut Text,
+    ) {
+        str::render_as_text_update(self.borrow(), renderer, text)
+    }
+}
