87095522ace130d11afe2efc07972c4bf00c0497
Equal Ma <equalma@outlook.com>
2024-04-25 17:42:08 +0800

    chore: rename

---
+++ crates/frender-ssr/src/elements/mod.rs
@@ -0,0 +1,12 @@
+pub mod array;
+pub mod keyed;
+
+mod boxed;
+mod char;
+mod option;
+mod scalar;
+mod str;
+mod tuple;
+
+#[cfg(feature = "either")]
+mod either;
--- crates/frender-ssr/src/lib.rs
+++ crates/frender-ssr/src/lib.rs
@@ -1,9 +1,9 @@
 pub use frender_ssr_html as html;
 
-mod trait_element;
-pub use trait_element::*;
+mod ssr_element;
+pub use ssr_element::*;
 
 mod ssr_ext;
 pub use ssr_ext::*;
 
-pub mod element;
+pub mod elements;
