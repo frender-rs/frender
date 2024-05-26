9ff33c4e50238505ba8cdb48188e98069db62976
Equal Ma <equalma@outlook.com>
2024-05-26 16:07:12 +0800

    feat: MaybeStringValue

--- crates/frender-html-common/src/lib.rs
+++ crates/frender-html-common/src/lib.rs
@@ -18,7 +18,7 @@ mod spellcheck;
 pub use spellcheck::Spellcheck;
 
 mod string_value;
-pub use string_value::StringValue;
+pub use string_value::{EitherStringValue, MaybeStringValue, StringValue};
 
 mod into_one_string_or_empty;
 pub use into_one_string_or_empty::IntoOneStringOrEmpty;
