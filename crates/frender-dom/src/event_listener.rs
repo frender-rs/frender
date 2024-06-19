05ae3be4db9f3682e7dd73c3f36e41ac9b6e4e81
Equal Ma <equalma@outlook.com>
2024-06-20 01:07:45 +0800

    feat!: OnEvent associated types now require `'static`
    This is for the future feature RenderWith

--- crates/frender-dom/src/event_listener.rs
+++ crates/frender-dom/src/event_listener.rs
@@ -5,10 +5,12 @@ use crate::event_types::EventType;
 
 pub trait OnEvent<Renderer: ?Sized, ET: EventType> {
     type EventListener<F: HandleEvent<ET::Event> + 'static>: Default
+        + 'static
         + RegisterOrUpdate<Self, Renderer, F>;
 
     type EventListenerUnpinned<F: HandleEvent<ET::Event> + 'static>: Default
         + Unpin
+        + 'static
         + RegisterOrUpdate<Self, Renderer, F>;
 }
 
