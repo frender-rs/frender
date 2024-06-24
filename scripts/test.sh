251bee8eacf26f2eead90c027e2317bdbb9a034b
Equal Ma <equalma@outlook.com>
2024-06-24 23:09:05 +0800

    chore: update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,3 +3,6 @@ cargo test -p frender-ssr-html --features either
 
 cargo test -p frender-ssr
 cargo test -p frender-ssr --features either
+
+cargo test -p frender-render-with
+cargo +nightly test -p frender-render-with --features nightly
