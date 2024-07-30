0ffd6f3fbfce9a481df8fa2f25eddb00de3c05d9
Equal Ma <equalma@outlook.com>
2024-07-31 01:16:46 +0800

    feat: MemoPhantom and MemoPhantomAndProvideFirstArgument

--- packages/frender/src/memoed.rs
+++ packages/frender/src/memoed.rs
@@ -28,6 +28,34 @@ impl<F, Dep> Memo<F, Dep> {
     }
 }
 
+/// As [`CsrElement`](crate::CsrElement), this type has the same
+/// [`RenderStateKind`](crate::CsrElement::RenderStateKind) as [`Memo<F, Dep>`].
+///
+/// It will always try to update render state with the element returned by `F`.
+///
+/// If the `Dep` hasn't been initialized with [`Memo<F, Dep>`]:
+///     - if `SKIP_IF_DEP_IS_NONE` is `false`: panic
+///     - if `SKIP_IF_DEP_IS_NONE` is `true`: silently skip (`F` is not called and the render state is not updated)
+pub struct MemoPhantom<
+    F: for<'a> crate::FnOnceOutputElement<&'a Dep>,
+    Dep,
+    const SKIP_IF_DEP_IS_NONE: bool,
+> {
+    pub f: F,
+    _dep: std::marker::PhantomData<Dep>,
+}
+
+impl<F: for<'a> crate::FnOnceOutputElement<&'a Dep>, Dep, const SKIP_IF_DEP_IS_NONE: bool>
+    MemoPhantom<F, Dep, SKIP_IF_DEP_IS_NONE>
+{
+    pub const fn new(f: F) -> Self {
+        Self {
+            f,
+            _dep: std::marker::PhantomData,
+        }
+    }
+}
+
 mod element {
     mod ssr {
         use frender_ssr::SsrElement;
@@ -58,7 +86,9 @@ mod element {
 
         use crate::{FnOnce2OutputElement, FnOnceOutputElement};
 
-        use super::super::{Memo, MemoAndProvideFirstArgument};
+        use super::super::{
+            Memo, MemoAndProvideFirstArgument, MemoPhantom, MemoPhantomAndProvideFirstArgument,
+        };
 
         enum Never {}
         pub struct Kind<K, Dep>(Never, std::marker::PhantomData<(K, Dep)>);
@@ -167,6 +197,185 @@ mod element {
 
             frender_html::proxy_csr_element!(|this| this.into_memo());
         }
+
+        fn skip_or_panic<const SKIP: bool>() {
+            struct ConstBool<const V: bool>;
+
+            trait ConstNot {
+                const NOT: bool;
+            }
+
+            impl<const V: bool> ConstNot for ConstBool<V> {
+                const NOT: bool = !V;
+            }
+
+            if ConstBool::<SKIP>::NOT {
+                panic!(
+                    "memoed dependency has not been initialized by Memo* but accessed by MemoPhantom*"
+                )
+            }
+        }
+
+        impl<F, Dep, K: RenderStateKind, const SKIP_IF_DEP_IS_NONE: bool> CsrElement
+            for MemoPhantom<F, Dep, SKIP_IF_DEP_IS_NONE>
+        where
+            F: for<'a> FnOnceOutputElement<&'a Dep, OutputElementRenderStateKind = K>,
+        {
+            type RenderStateKind = crate::memoed::Kind<K, Dep>;
+
+            fn render_update<Ctx: ?Sized + frender_html::HtmlRenderContext>(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: std::pin::Pin<
+                    &mut frender_html::RenderStateOfContext<Self::RenderStateKind, Ctx>,
+                >,
+            ) where
+                Self: Sized,
+            {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state.pin_project();
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep).render_update(render_context, render_state)
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+
+            fn render_update_force_reposition<Ctx: ?Sized + frender_html::HtmlRenderContext>(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: std::pin::Pin<
+                    &mut frender_html::RenderStateOfContext<Self::RenderStateKind, Ctx>,
+                >,
+            ) where
+                Self: Sized,
+            {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state.pin_project();
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep).render_update_force_reposition(render_context, render_state)
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+
+            fn render_update_maybe_reposition<Ctx: ?Sized + frender_html::HtmlRenderContext>(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: std::pin::Pin<
+                    &mut frender_html::RenderStateOfContext<Self::RenderStateKind, Ctx>,
+                >,
+                force_reposition: bool,
+            ) {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state.pin_project();
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep).render_update_maybe_reposition(
+                        render_context,
+                        render_state,
+                        force_reposition,
+                    )
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+
+            fn unpinned_render_update<Ctx: ?Sized + frender_html::HtmlRenderContext>(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: &mut frender_html::UnpinnedRenderStateOfContext<
+                    Self::RenderStateKind,
+                    Ctx,
+                >,
+            ) where
+                Self: Sized,
+            {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state;
+
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep).unpinned_render_update(render_context, render_state)
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+
+            fn unpinned_render_update_force_reposition<
+                Ctx: ?Sized + frender_html::HtmlRenderContext,
+            >(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: &mut frender_html::UnpinnedRenderStateOfContext<
+                    Self::RenderStateKind,
+                    Ctx,
+                >,
+            ) where
+                Self: Sized,
+            {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state;
+
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep)
+                        .unpinned_render_update_force_reposition(render_context, render_state)
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+
+            fn unpinned_render_update_maybe_reposition<
+                Ctx: ?Sized + frender_html::HtmlRenderContext,
+            >(
+                //
+                self,
+                render_context: &mut Ctx,
+                render_state: &mut frender_html::UnpinnedRenderStateOfContext<
+                    Self::RenderStateKind,
+                    Ctx,
+                >,
+                force_reposition: bool,
+            ) {
+                let CompoundState {
+                    reactive: render_state,
+                    non_reactive: dep,
+                } = render_state;
+
+                if let Some(dep) = dep.as_ref() {
+                    (self.f)(dep).unpinned_render_update_maybe_reposition(
+                        render_context,
+                        render_state,
+                        force_reposition,
+                    )
+                } else {
+                    skip_or_panic::<SKIP_IF_DEP_IS_NONE>()
+                }
+            }
+        }
+
+        impl<F, V, Dep, K: RenderStateKind, const SKIP_IF_DEP_IS_NONE: bool> CsrElement
+            for MemoPhantomAndProvideFirstArgument<F, V, Dep, SKIP_IF_DEP_IS_NONE>
+        where
+            F: for<'a> FnOnce2OutputElement<V, &'a Dep, OutputElementRenderStateKind = K>,
+        {
+            type RenderStateKind = crate::memoed::Kind<K, Dep>;
+
+            frender_html::proxy_csr_element!(|this| this.f);
+        }
     }
 }
 
@@ -197,5 +406,49 @@ where
     {
         let (f, dep) = self.into_f_and_dep();
         Memo(f, dep)
+    }
+}
+
+/// Works like [`MemoPhantom`]
+pub struct MemoPhantomAndProvideFirstArgument<
+    F: for<'a> crate::FnOnce2OutputElement<A, &'a Dep>,
+    A,
+    Dep,
+    const SKIP_IF_DEP_IS_NONE: bool,
+> {
+    pub f: F,
+    pub first_argument: A,
+    _dep: std::marker::PhantomData<Dep>,
+}
+
+impl<
+        F: for<'a> crate::FnOnce2OutputElement<V, &'a Dep>,
+        V,
+        Dep,
+        const SKIP_IF_DEP_IS_NONE: bool,
+    > MemoPhantomAndProvideFirstArgument<F, V, Dep, SKIP_IF_DEP_IS_NONE>
+{
+    pub const fn new(f: F, first_argument: V) -> Self {
+        Self {
+            f,
+            first_argument,
+            _dep: std::marker::PhantomData,
+        }
+    }
+
+    fn into_f(
+        self,
+    ) -> impl FnOnce(&Dep) -> <F as crate::FnOnce2OutputElement<V, &Dep>>::OutputElement {
+        move |dep: &_| (self.f)(self.first_argument, dep)
+    }
+
+    pub fn into_memo_phantom(
+        self,
+    ) -> MemoPhantom<
+        impl FnOnce(&Dep) -> <F as crate::FnOnce2OutputElement<V, &Dep>>::OutputElement,
+        Dep,
+        SKIP_IF_DEP_IS_NONE,
+    > {
+        MemoPhantom::new(self.into_f())
     }
 }
