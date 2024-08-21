b3b31d3cc989f500c801c28add23498b14a955db
Equal Ma <equalma@outlook.com>
2024-08-22 00:20:13 +0800

    feat: export style

--- packages/frender/src/lib.rs
+++ packages/frender/src/lib.rs
@@ -83,6 +83,9 @@ pub use frender_html_common::dom_tokens::{
     ChainableDomTokens, DomTokenList, DomTokens,
 };
 
+pub use frender_style as style;
+pub use frender_style::{chain_styles, const_style, style};
+
 pub mod memoed;
 pub use memoed::{memo, Memo};
 
@@ -118,6 +121,8 @@ pub mod prelude {
     pub use crate::{Elements, Keyed};
 
     pub use frender_html_common::dom_tokens::{dom_tokens, ChainableDomTokens, DomTokens};
+
+    pub use frender_style::style;
 
     pub use crate::elements;
 
