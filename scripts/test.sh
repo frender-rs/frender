c7ed4937fcbde5166fd12553903b50f440066f11
Equal Ma <equalma@outlook.com>
2024-08-31 02:44:42 +0800

    test: frender-style

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,9 @@ cargo test -p frender-dom-tokens
 cargo test -p frender-dom-tokens --features either
 cargo test -p frender-dom-tokens --features web
 
+cargo test -p frender-style
+cargo test -p frender-style --features either
+cargo test -p frender-style --features web
+
 cargo test -p frender-render-with
 cargo +nightly test -p frender-render-with --features nightly
