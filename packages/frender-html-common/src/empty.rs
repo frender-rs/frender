32ecabceafa9293544e57af46944fdf15277a712
Equal Ma <equalma@outlook.com>
2024-09-10 18:48:08 +0800

    feat!: remove frender-html-common

--- packages/frender-html-common/src/empty.rs
+++
@@ -1,9 +0,0 @@
-use frender_common::Empty;
-
-impl crate::MaybeStringValue for Empty {
-    type StringValue = async_str_iter::never::Never;
-
-    fn maybe_string_value(Self: Self) -> Option<Self::StringValue> {
-        None
-    }
-}
--- packages/frender-html-common/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod string_value;
-pub use string_value::{EitherStringValue, MaybeStringValue, StringValue};
-
-mod empty;
