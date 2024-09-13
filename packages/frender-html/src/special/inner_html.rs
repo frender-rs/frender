5075cc0bec7a41d07fdfd5a0f5ae5459c6fe685e
Equal Ma <equalma@outlook.com>
2024-09-14 07:55:52 +0800

    chore: remove unused code

--- packages/frender-html/src/special/inner_html.rs
+++ packages/frender-html/src/special/inner_html.rs
@@ -1,26 +1,8 @@
 mod csr {
-    use frender_attr_value::csr::UpdateAttrValue;
     use frender_common::strings::{csr::update_with_option_cache, CsrStr};
     use frender_dom::{behaviors::Element as _, render_state::non_reactive::NonReactiveRenderState, special::DangerousInnerHtml};
 
     use crate::{kinds::KindOfNonReactive, CsrComponent, CsrComponentNormalElement};
-
-    struct UpdateElementInnerHtml<'a, E: ?Sized, R: ?Sized> {
-        element: &'a mut E,
-        renderer: &'a mut R,
-    }
-
-    impl<'a, E: ?Sized + frender_dom::behaviors::Element<R>, R: ?Sized> UpdateAttrValue for UpdateElementInnerHtml<'a, E, R> {
-        type Kind = str;
-
-        fn set(self, value: &str) {
-            self.element.set_inner_html(self.renderer, value)
-        }
-
-        fn remove(self) {
-            self.element.set_inner_html(self.renderer, "")
-        }
-    }
 
     impl<C: CsrComponentNormalElement, S: CsrStr> CsrComponent<DangerousInnerHtml<S>> for C {
         type ChildrenRenderStateKind = KindOfNonReactive<Option<S::StaticStrCache>>;
