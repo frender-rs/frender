0f854be69ff687c65c264f2d4b2f32ba0847c8d3
Equal Ma <equalma@outlook.com>
2024-11-20 16:19:36 +0800

    feat!: frender_html::elements::str

--- packages/frender-html/src/elements/str.rs
+++ packages/frender-html/src/elements/str.rs
@@ -1,14 +1,12 @@
-use std::borrow::{Borrow, Cow};
+use std::borrow::Cow;
 
-// use wasm_bindgen::{JsCast, JsValue};
-
-use frender_dom::render::{RenderContext, RenderWithContext};
+use frender_common::ToAsRefStr;
+use frender_dom::render::{RenderContext, RenderTextFromKnown, RenderWithContext};
 use frender_dom::string_element::StringElement;
 
-use crate::dom::render::RenderTextFrom;
 use crate::{dom::behaviors::Node, RenderHtml};
 
-use crate::{Element, HtmlRenderContext, RenderState, RenderStateOfContext};
+use crate::{Element, HtmlRenderContext, RenderState};
 
 /// `Text` node with a field recording whether it is unmounted.
 pub struct TextNode<Text> {
@@ -39,15 +37,6 @@ impl<Text> TextNode<Text> {
         render_context.map_mut_render_context(|render_context| node.readd_self(render_context, true));
         Self { node, unmounted: false }
     }
-
-    fn mount_from<Ctx: ?Sized + RenderContext, V: ?Sized>(render_context: &mut Ctx, v: &V) -> Self
-    where
-        Ctx::Renderer: RenderTextFrom<Text, V>,
-        Text: Node<Ctx::Renderer>,
-    {
-        let node = render_context.renderer_mut().render_text_from(v);
-        Self::mount(render_context, node)
-    }
 }
 
 pub struct State<Cache, Text> {
@@ -55,121 +44,46 @@ pub struct State<Cache, Text> {
     cache: Cache,
 }
 
-trait BorrowText: Borrow<Self::Text> {
-    type Text: ?Sized;
-}
-
-frender_common::impl_many!(
-    impl<__> BorrowText
-        for each_of![
-            //
-            &'static str,
-            String,
-            Cow<'static, str>,
-            std::rc::Rc<str>,
-            std::sync::Arc<str>,
-        ]
-    {
-        type Text = str;
-    }
-);
-
-frender_common::impl_many!(
-    impl<__> BorrowText
-        for each_of![
-            StringElement,
-            // scalar types
-            i8,
-            u8,
-            i16,
-            u16,
-            i32,
-            u32,
-            i64,
-            u64,
-            i128,
-            u128,
-            isize,
-            usize,
-            f32,
-            f64,
-            char,
-        ]
-    {
-        type Text = Self;
-    }
-);
-
-trait FnOnceOutputBorrow<Arg, V: ?Sized>: FnOnce(Arg) -> Self::OutputBorrow {
-    type OutputBorrow: Borrow<V>;
-}
-
-impl<Arg, V: ?Sized, F: FnOnce(Arg) -> R, R: Borrow<V>> FnOnceOutputBorrow<Arg, V> for F {
-    type OutputBorrow = R;
-}
-
 impl<Cache, Text> State<Cache, Text> {
-    fn update_with_str_maybe_reposition<Ctx: ?Sized + RenderContext, S: Borrow<V>, V: ?Sized>(
-        &mut self,
-        data: S,
-        render_context: &mut Ctx,
-        force_reposition: bool,
-        not_match_cache: impl FnOnce(&S, &Cache) -> bool,
-        update_cache: impl FnOnce(S, &mut Cache),
-    ) where
-        Ctx::Renderer: RenderHtml<Text = Text> + RenderTextFrom<Text, V>,
-        Text: Node<Ctx::Renderer>,
-    {
-        self.update_maybe_reposition::<Ctx, S, V>(data, render_context, force_reposition, Borrow::borrow, not_match_cache, update_cache)
-    }
-
-    fn update_maybe_reposition<Ctx: ?Sized + RenderContext, S, V: ?Sized>(
-        &mut self,
-        data: S,
-        render_context: &mut Ctx,
-        force_reposition: bool,
-        get_value: impl for<'a> FnOnceOutputBorrow<&'a S, V>,
-        not_match_cache: impl FnOnce(&S, &Cache) -> bool,
-        update_cache: impl FnOnce(S, &mut Cache),
-    ) where
-        Ctx::Renderer: RenderHtml<Text = Text> + RenderTextFrom<Text, V>,
-        Text: Node<Ctx::Renderer>,
-    {
-        if not_match_cache(&data, &self.cache) {
-            render_context.renderer_mut().update_text_from(&mut self.text_node.node, get_value(&data).borrow());
-
-            update_cache(data, &mut self.cache);
-        }
-
-        render_context.map_mut_render_context(|render_context| self.text_node.readd_self(render_context, force_reposition))
-    }
-
-    pub fn initialize_with_str<Ctx: ?Sized + RenderContext, S: Borrow<V>, V: ?Sized>(data: S, render_context: &mut Ctx, create_cache: impl FnOnce(S) -> Cache) -> Self
-    where
-        Ctx::Renderer: RenderHtml<Text = Text> + RenderTextFrom<Text, V>,
-        Text: Node<Ctx::Renderer>,
-    {
-        Self::init::<Ctx, S, V>(data, render_context, Borrow::borrow, create_cache)
-    }
-
-    fn init<Ctx: ?Sized + RenderContext, S, V: ?Sized>(
+    pub fn init_with<S: RenderAsTextWithCache<Cache = Cache>, Ctx: ?Sized + RenderContext>(
         //
         data: S,
         render_context: &mut Ctx,
-        get_value: impl for<'a> FnOnceOutputBorrow<&'a S, V>,
-        create_cache: impl FnOnce(S) -> Cache,
     ) -> Self
     where
-        Ctx::Renderer: RenderHtml<Text = Text> + RenderTextFrom<Text, V>,
+        Ctx::Renderer: RenderHtml<Text = Text>,
         Text: Node<Ctx::Renderer>,
     {
+        let (text, cache) = data.render_as_text_with_cache(render_context.renderer_mut());
         State {
             text_node: {
-                let text_node = TextNode::mount_from(render_context, get_value(&data).borrow());
+                let text_node = TextNode::mount(render_context, text);
                 text_node
             },
-            cache: create_cache(data),
+            cache,
         }
+    }
+
+    pub fn update_maybe_reposition_with<S: RenderAsTextWithCache<Cache = Cache>, Ctx: ?Sized + RenderContext>(
+        //
+        &mut self,
+        data: S,
+        render_context: &mut Ctx,
+        force_reposition: bool,
+    ) where
+        Ctx::Renderer: RenderHtml<Text = Text>,
+        Text: Node<Ctx::Renderer>,
+    {
+        if S::not_match_cache(&data, &self.cache) {
+            data.render_update_as_text_with_cache(
+                //
+                render_context.renderer_mut(),
+                &mut self.text_node.node,
+                &mut self.cache,
+            )
+        }
+
+        render_context.map_mut_render_context(|render_context| self.text_node.readd_self(render_context, force_reposition))
     }
 }
 
@@ -198,6 +112,17 @@ impl<Cache, Text: Node<R>, R: ?Sized> RenderState<R> for State<Cache, Text> {
     }
 }
 
+pub fn unpinned_render_update_maybe_reposition<E: RenderAsTextWithCache, Ctx: ?Sized + HtmlRenderContext>(
+    el: E,
+    render_context: &mut Ctx,
+    render_state: &mut Option<State<E::Cache, <Ctx::Renderer as RenderHtml>::Text>>,
+    force_reposition: bool,
+) {
+    match render_state {
+        Some(render_state) => render_state.update_maybe_reposition_with::<E, Ctx>(el, render_context, force_reposition),
+        render_state @ None => *render_state = Some(State::init_with::<E, Ctx>(el, render_context)),
+    }
+}
 pub struct Kind<Cache: 'static>(super::Kind<Cache>);
 
 impl<Cache: 'static> crate::RenderStateKindPinned for Kind<Cache> {
@@ -207,18 +132,19 @@ impl<Cache: 'static> crate::RenderStateKindUnpinned for Kind<Cache> {
     type UnpinnedRenderState<R: RenderHtml + ?Sized> = Option<State<Cache, R::Text>>;
 }
 
+// impl<T: known RenderAsTextWithSelfAsCache> Element for T {}
 frender_common::impl_many!(
     impl<__> Element
         for each_of![
-            // static strings
+            // known static strings: impl KnownStaticStr -> impl RenderAsTextWithSelfAsCache
             Cow<'static, str>,
             &'static str,
             String,
             std::rc::Rc<str>,
             std::sync::Arc<str>,
-            //
+            // - known special text elements
             StringElement,
-            // scalar types
+            // - known scalar types
             i8,
             u8,
             i16,
@@ -238,51 +164,196 @@ frender_common::impl_many!(
     {
         type RenderStateKind = Kind<Self>;
 
-        fn render_update_maybe_reposition<Ctx: ?Sized + HtmlRenderContext>(self, render_context: &mut Ctx, render_state: std::pin::Pin<&mut RenderStateOfContext<Self::RenderStateKind, Ctx>>, force_reposition: bool) {
-            match render_state.get_mut() {
-                Some(render_state) => render_state.update_with_str_maybe_reposition::<Ctx, Self, <Self as BorrowText>::Text>(self, render_context, force_reposition, PartialEq::ne, |this, cache| *cache = this),
-                render_state @ None => *render_state = Some(State::initialize_with_str::<Ctx, Self, <Self as BorrowText>::Text>(self, render_context, std::convert::identity)),
-            }
+        fn unpinned_render_update_maybe_reposition<Ctx: ?Sized + HtmlRenderContext>(
+            //
+            self,
+            render_context: &mut Ctx,
+            render_state: &mut crate::UnpinnedRenderStateOfContext<Self::RenderStateKind, Ctx>,
+            force_reposition: bool,
+        ) {
+            unpinned_render_update_maybe_reposition(
+                //
+                self,
+                render_context,
+                render_state,
+                force_reposition,
+            )
         }
 
-        crate::impl_unpinned_render_for_unpin! {}
+        crate::impl_render_for_unpin! {}
     }
 );
 
-/// <code>where TempStr<S>: [CsrStr](frender_common::strings::CsrStr)</code>
+/// <code>where TempStr\<S>: [CsrStr](frender_common::strings::CsrStr)</code>
+///
+/// `impl CsrStr` -> `impl RenderAsTextWithCache` -> `impl Element`
 impl<S> Element for frender_common::TempStr<S>
 where
     S: frender_common::IntoStaticStrCache,
 {
     type RenderStateKind = Kind<S::StaticStrCache>;
 
-    fn render_update_maybe_reposition<Ctx: ?Sized + HtmlRenderContext>(self, render_context: &mut Ctx, render_state: std::pin::Pin<&mut RenderStateOfContext<Self::RenderStateKind, Ctx>>, force_reposition: bool) {
-        fn to_as_ref_str_and_borrow(v: &impl frender_common::ToAsRefStr) -> impl '_ + Borrow<str> {
-            let v = frender_common::ToAsRefStr::to_as_ref_str(v);
-            struct BorrowAsRef<S>(S);
-
-            impl<S: AsRef<str>> Borrow<str> for BorrowAsRef<S> {
-                fn borrow(&self) -> &str {
-                    self.0.as_ref()
-                }
-            }
-
-            BorrowAsRef(v)
-        }
-
-        match render_state.get_mut() {
-            Some(render_state) => {
-                if render_state.cache != self.0 {
-                    use frender_common::ToAsRefStr as _;
-                    self.0.update_into_static_str_cache(&mut render_state.cache);
-                    render_context.renderer_mut().update_text_from(&mut render_state.text_node.node, render_state.cache.to_as_ref_str().as_ref());
-                }
-
-                render_context.map_mut_render_context(|render_context| render_state.text_node.readd_self(render_context, force_reposition))
-            }
-            render_state @ None => *render_state = Some(State::init(self.0.into_static_str_cache(), render_context, to_as_ref_str_and_borrow, std::convert::identity)),
-        }
+    fn unpinned_render_update_maybe_reposition<Ctx: ?Sized + HtmlRenderContext>(
+        //
+        self,
+        render_context: &mut Ctx,
+        render_state: &mut crate::UnpinnedRenderStateOfContext<Self::RenderStateKind, Ctx>,
+        force_reposition: bool,
+    ) {
+        unpinned_render_update_maybe_reposition(self, render_context, render_state, force_reposition)
     }
 
-    crate::impl_unpinned_render_for_unpin! {}
+    crate::impl_render_for_unpin! {}
 }
+
+pub trait RenderAsTextWithCache {
+    type Cache: 'static;
+
+    fn not_match_cache(&self, cache: &Self::Cache) -> bool;
+
+    /// This method will only be called on initialization.
+    fn render_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+    ) -> (Renderer::Text, Self::Cache);
+
+    /// This method will only be called on [cache mismatch](RenderAsTextWithCache::not_match_cache).
+    /// The argument `cache` is stale and the implementation should update it.
+    fn render_update_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+        text_handle: &mut Renderer::Text,
+        cache: &mut Self::Cache,
+    );
+}
+
+impl<T: RenderAsTextWithSelfAsCache> RenderAsTextWithCache for T {
+    type Cache = T;
+
+    fn not_match_cache(&self, cache: &Self::Cache) -> bool {
+        self != cache
+    }
+
+    fn render_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+    ) -> (Renderer::Text, Self::Cache) {
+        (self.render_as_text(renderer), self)
+    }
+
+    fn render_update_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+        text_handle: &mut Renderer::Text,
+        cache: &mut Self::Cache,
+    ) {
+        self.render_update_as_text(renderer, text_handle);
+        *cache = self;
+    }
+}
+
+/// <code>where TempStr\<S>: [CsrStr](frender_common::strings::CsrStr)</code>
+impl<S> RenderAsTextWithCache for frender_common::TempStr<S>
+where
+    S: frender_common::IntoStaticStrCache,
+{
+    type Cache = S::StaticStrCache;
+
+    fn not_match_cache(&self, cache: &Self::Cache) -> bool {
+        *cache != self.0
+    }
+
+    fn render_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+    ) -> (Renderer::Text, Self::Cache) {
+        let cache = self.0.into_static_str_cache();
+
+        let text = {
+            let s = cache.to_as_ref_str();
+            renderer.render_text_from(s.as_ref())
+        };
+
+        (text, cache)
+    }
+
+    fn render_update_as_text_with_cache<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+        text_handle: &mut Renderer::Text,
+        cache: &mut Self::Cache,
+    ) {
+        self.0.update_into_static_str_cache(cache);
+        let s = cache.to_as_ref_str();
+        renderer.update_text_from(text_handle, s.as_ref());
+    }
+}
+
+trait RenderAsTextWithSelfAsCache: 'static + PartialEq {
+    fn render_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R) -> Text;
+    fn render_update_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R, text_handle: &mut Text);
+}
+
+impl<S: KnownStaticStr> RenderAsTextWithSelfAsCache for S {
+    fn render_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R) -> Text {
+        renderer.render_text_from(self.as_ref())
+    }
+
+    fn render_update_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R, text_handle: &mut Text) {
+        renderer.update_text_from(text_handle, self.as_ref())
+    }
+}
+
+frender_common::impl_many!(
+    impl<__> RenderAsTextWithSelfAsCache
+        for each_of![
+            StringElement,
+            // scalar types
+            i8,
+            u8,
+            i16,
+            u16,
+            i32,
+            u32,
+            i64,
+            u64,
+            i128,
+            u128,
+            isize,
+            usize,
+            f32,
+            f64,
+            char,
+        ]
+    {
+        fn render_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R) -> Text {
+            renderer.render_text_from(self)
+        }
+
+        fn render_update_as_text<Text, R: ?Sized + RenderTextFromKnown<Text>>(&self, renderer: &mut R, text_handle: &mut Text) {
+            renderer.update_text_from(text_handle, self);
+        }
+    }
+);
+
+trait KnownStaticStr: 'static + AsRef<str> + PartialEq {}
+
+frender_common::impl_many!(
+    impl<__> KnownStaticStr
+        for each_of![
+            //
+            &'static str,
+            String,
+            Cow<'static, str>,
+            std::rc::Rc<str>,
+            std::sync::Arc<str>,
+        ]
+    {
+    }
+);
