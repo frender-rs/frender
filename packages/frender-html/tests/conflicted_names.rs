134f27e560585ce4e9051413a90fd2574d35a40a
Equal Ma <equalma@outlook.com>
2024-11-24 20:58:58 +0800

    chore: add more tests for props builder method with conflicted names

--- packages/frender-html/tests/conflicted_names.rs
+++ packages/frender-html/tests/conflicted_names.rs
@@ -1,9 +1,30 @@
 use frender_html::cs;
 
 #[test]
-fn height() {
-    _ = cs::canvas().height(1);
-    _ = cs::td().height("1");
+fn height_weight() {
+    // ElementWithHeightWidthU32Attributes
+    _ = cs::video().width(100).height(50);
+    _ = cs::canvas().height(1).width(2);
+    _ = cs::img().height(100).width(250);
+    _ = cs::input().width(40).height(10);
+    _ = cs::source().width(40).height(10);
+
+    // ElementWithHeightWidthStrAttributes
+    _ = cs::embed().height("10").width("40");
+    _ = cs::iframe().height("10").width("40");
+    _ = cs::object().height("10").width("40");
+    _ = cs::td().width("2").height("1");
+    _ = cs::th().height("2").width("1");
+}
+
+#[test]
+fn max_min() {
+    // ElementWithMaxF64Attribute
+    _ = cs::progress().max(1.);
+    _ = cs::meter().max(1.).min(0.);
+
+    // HtmlInputElement
+    _ = cs::input().max("1").min("0");
 }
 
 #[test]
