11a5f0065a278e4bc23e00c419260ac523e85dc9
Equal Ma <equalma@outlook.com>
2025-02-06 13:20:36 +0800

    feat!: DangerousInnerHtml should not impl SsrElement

--- packages/frender-dom/src/special/inner_html.rs
+++ packages/frender-dom/src/special/inner_html.rs
@@ -1,20 +1,10 @@
-use async_str_iter::any_str::IterAnyStr;
-use frender_common::{strings::SsrStr, IntoStaticStr};
-use frender_ssr::{html::verbatim_html::DangerousVerbatimHtml, SsrElement};
-
-/// In `csr`, this sets `element.innerHTML = self.0` only if `self.0` changed.
+/// In `csr`, this sets `element.innerHTML = self.0`.
 /// Invalid html will cause [`SyntaxError`](https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#syntaxerror).
 ///
-/// In `ssr`, this uses [`DangerousVerbatimHtml`] which is more dangerous than `csr`
-/// because invalid html is allowed.
+/// For now, `DangerousInnerHtml` doesn't implement [SsrElement] because
+/// frender doesn't want to include a html validator.
+/// Implement [SsrElement] for your custom type with [`DangerousVerbatimHtml`] if ssr is needed.
+///
+/// [`SsrElement`]: frender_ssr::SsrElement
+/// [`DangerousVerbatimHtml`]: frender_ssr::html::verbatim_html::DangerousVerbatimHtml
 pub struct DangerousInnerHtml<S>(pub S);
-
-impl<S: SsrStr> SsrElement for DangerousInnerHtml<S> {
-    type HtmlChildren = DangerousVerbatimHtml<IterAnyStr<S::StaticStr>>;
-
-    fn into_html_children(self) -> Self::HtmlChildren {
-        DangerousVerbatimHtml::new(IterAnyStr::new(
-            self.0.into_into_static_str().into_static_str(),
-        ))
-    }
-}
