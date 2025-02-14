98b0839b84db937c9668974825f32a83c6ab56d7
Equal Ma <equalma@outlook.com>
2025-02-15 00:36:56 +0800

    fix: frender-context

--- packages/frender-context/src/element/csr.rs
+++ packages/frender-context/src/element/csr.rs
@@ -1,19 +1,18 @@
-use std::{marker::PhantomData, task::Poll};
+use std::{marker::PhantomData, pin::Pin, task::Poll};
 
 use frender_html::{
     experimental::{
-        self, PinnedRenderStateKind, PinnedRenderStateKindPollRender, RenderStates,
+        self, PinnedRenderStateKind, PinnedRenderStateKindPollRender, RenderInitPinned,
         UnpinnedRenderStateKind, UnpinnedRenderStateKindPollRender,
     },
-    kinds::UiHandleWithNonReactiveState,
-    CsrElement, RenderHtml,
+    CsrElement, RenderHtml, StateUnmount,
 };
 
 use crate::{ContextKey, ContextKeyInner};
 
 use super::{ElementWithContext, IntoContextValue};
 
-pub struct ContextKeyAndValue<C: ContextKeyInner + 'static> {
+struct ContextKeyAndValue<C: ContextKeyInner + 'static> {
     context_key: &'static ContextKey<C>,
     value: C::SwapValue,
 }
@@ -37,6 +36,24 @@ impl<C: ContextKeyInner + 'static> ContextKeyAndValue<C> {
     }
 }
 
+pin_project_lite::pin_project!(
+    #[project = StateProj]
+    pub struct State<S, C: ContextKeyInner>
+    where
+        C: 'static,
+    {
+        #[pin]
+        state: S,
+        context_key_and_value: ContextKeyAndValue<C>,
+    }
+);
+
+impl<S: StateUnmount, C: ContextKeyInner> StateUnmount for State<S, C> {
+    fn state_unmount(self: Pin<&mut Self>) {
+        self.project().state.state_unmount()
+    }
+}
+
 // region: kind
 
 enum Never {}
@@ -45,10 +62,8 @@ pub struct Kind<C: ContextKeyInner + 'static, K>(Never, PhantomData<(C, K)>);
 impl<C: ContextKeyInner + 'static, K: UnpinnedRenderStateKind> UnpinnedRenderStateKind
     for Kind<C, K>
 {
-    type UnpinnedUiHandle<R: RenderHtml + ?Sized> =
-        UiHandleWithNonReactiveState<K::UnpinnedUiHandle<R>, ContextKeyAndValue<C>>;
-    type UnpinnedNonReactiveState<R: RenderHtml + ?Sized> = K::UnpinnedNonReactiveState<R>;
-    type UnpinnedReactiveState = K::UnpinnedReactiveState;
+    type UnpinnedUiHandle<R: RenderHtml + ?Sized> = K::UnpinnedUiHandle<R>;
+    type UnpinnedState<R: RenderHtml + ?Sized> = State<K::UnpinnedState<R>, C>;
 }
 
 impl<C: ContextKeyInner, K: UnpinnedRenderStateKindPollRender> UnpinnedRenderStateKindPollRender
@@ -57,36 +72,20 @@ impl<C: ContextKeyInner, K: UnpinnedRenderStateKindPollRender> UnpinnedRenderSta
     fn unpinned_poll_render<R: RenderHtml + ?Sized>(
         //
         renderer: &mut R,
-        RenderStates {
-            ui_handle:
-                UiHandleWithNonReactiveState {
-                    ui_handle,
-                    non_reactive_state: context_key_and_value,
-                },
-            non_reactive_state,
-            reactive_state,
-        }: experimental::UnpinnedMutRenderStatesOfKind<Self, R>,
+        State {
+            state,
+            context_key_and_value,
+        }: &mut Self::UnpinnedState<R>,
+        ui_handle: &mut Self::UnpinnedUiHandle<R>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<()> {
-        context_key_and_value.provide(|| {
-            K::unpinned_poll_render(
-                renderer,
-                RenderStates {
-                    ui_handle,
-                    non_reactive_state,
-                    reactive_state,
-                },
-                cx,
-            )
-        })
+        context_key_and_value.provide(|| K::unpinned_poll_render(renderer, state, ui_handle, cx))
     }
 }
 
 impl<C: ContextKeyInner + 'static, K: PinnedRenderStateKind> PinnedRenderStateKind for Kind<C, K> {
-    type PinnedUiHandle<R: RenderHtml + ?Sized> =
-        UiHandleWithNonReactiveState<K::PinnedUiHandle<R>, ContextKeyAndValue<C>>;
-    type PinnedNonReactiveState<R: RenderHtml + ?Sized> = K::PinnedNonReactiveState<R>;
-    type PinnedReactiveState = K::PinnedReactiveState;
+    type PinnedUiHandle<R: RenderHtml + ?Sized> = K::PinnedUiHandle<R>;
+    type PinnedState<R: RenderHtml + ?Sized> = State<K::PinnedState<R>, C>;
 }
 
 impl<C: ContextKeyInner + 'static, K: PinnedRenderStateKindPollRender>
@@ -95,28 +94,16 @@ impl<C: ContextKeyInner + 'static, K: PinnedRenderStateKindPollRender>
     fn pinned_poll_render<R: RenderHtml + ?Sized>(
         //
         renderer: &mut R,
-        RenderStates {
-            ui_handle:
-                UiHandleWithNonReactiveState {
-                    ui_handle,
-                    non_reactive_state: context_key_and_value,
-                },
-            non_reactive_state,
-            reactive_state,
-        }: experimental::PinnedMutRenderStatesOfKind<Self, R>,
+        state: Pin<&mut Self::PinnedState<R>>,
+        ui_handle: &mut Self::PinnedUiHandle<R>,
         cx: &mut std::task::Context<'_>,
     ) -> Poll<()> {
-        context_key_and_value.provide(|| {
-            K::pinned_poll_render(
-                renderer,
-                RenderStates {
-                    ui_handle,
-                    non_reactive_state,
-                    reactive_state,
-                },
-                cx,
-            )
-        })
+        let StateProj {
+            state,
+            context_key_and_value,
+        } = state.project();
+
+        context_key_and_value.provide(|| K::pinned_poll_render(renderer, state, ui_handle, cx))
     }
 }
 // endregion
@@ -138,6 +125,21 @@ impl<T, Inner: 'static + ContextKeyInner<Value = T>, F: IntoContextValue<Context
     }
 }
 
+pub struct RenderInit<T>(pub T);
+
+impl<T: RenderInitPinned<R, S>, R, S, C: ContextKeyInner + 'static> RenderInitPinned<R, State<S, C>>
+    for RenderInit<T>
+{
+    type Output = T::Output;
+    fn render_init_pinned(self, renderer: R, state: Pin<&mut State<S, C>>) -> Self::Output {
+        let StateProj {
+            state,
+            context_key_and_value,
+        } = state.project();
+        context_key_and_value.provide(|| self.0.render_init_pinned(renderer, state))
+    }
+}
+
 impl<
         T,
         Inner: 'static + ContextKeyInner<Value = T>,
@@ -147,94 +149,137 @@ impl<
     > CsrElement for ElementWithContext<Inner, F, FE>
 {
     type RenderStateKind = Kind<Inner, E::RenderStateKind>;
+    type PinnedRenderInit<R: ?Sized + RenderHtml> = RenderInit<E::PinnedRenderInit<R>>;
 
-    fn pinned_render_init<Ctx: ?Sized + frender_html::HtmlRenderContext>(
+    fn pinned_render_init<Renderer: ?Sized + RenderHtml>(
         //
         self,
-        render_context: &mut Ctx,
-        states: experimental::PinMutRenderInitStatesOfKind<Self::RenderStateKind, Ctx::Renderer>,
-    ) -> experimental::PinnedUiHandleOfKind<Ctx::Renderer, Self::RenderStateKind> {
+        renderer: &mut Renderer,
+    ) -> (
+        //
+        experimental::PinnedStateOfKind<Renderer, Self::RenderStateKind>,
+        Self::PinnedRenderInit<Renderer>,
+    ) {
         let (mut context_key_and_value, get_element) = self.into_ctx_and_get_element();
-        let ui_handle = context_key_and_value
-            .provide(|| get_element().pinned_render_init(render_context, states));
+        let (state, render_init) =
+            context_key_and_value.provide(|| get_element().pinned_render_init(renderer));
 
-        UiHandleWithNonReactiveState {
-            ui_handle,
-            non_reactive_state: context_key_and_value,
-        }
+        (
+            State {
+                state,
+                context_key_and_value,
+            },
+            RenderInit(render_init),
+        )
     }
 
-    fn pinned_render_update<Ctx: ?Sized + frender_html::HtmlRenderContext>(
-        //
+    fn pinned_render_init_by_reusing<Ctx: ?Sized + frender_html::HtmlRenderContext>(
         self,
         render_context: &mut Ctx,
-        RenderStates {
-            ui_handle:
-                UiHandleWithNonReactiveState {
-                    ui_handle,
-                    non_reactive_state: context_key_and_value,
-                },
-            non_reactive_state,
-            reactive_state,
-        }: experimental::PinnedMutRenderStatesOfKind<Self::RenderStateKind, Ctx::Renderer>,
+        reused_state: Pin<
+            &mut experimental::PinnedStateOfKind<Ctx::Renderer, Self::RenderStateKind>,
+        >,
+        unmounted_ui_handle: experimental::PinnedUnmountedUiHandleOfKind<
+            Ctx::Renderer,
+            Self::RenderStateKind,
+        >,
+    ) -> experimental::PinnedUiHandleOfKind<Ctx::Renderer, Self::RenderStateKind> {
+        let Self {
+            context_key,
+            into_value,
+            get_element,
+        } = self;
+        let StateProj {
+            state,
+            context_key_and_value,
+        } = reused_state.project();
+        context_key_and_value.same_context_key_or_insert(context_key, into_value);
+        context_key_and_value.provide(|| {
+            get_element().pinned_render_init_by_reusing(render_context, state, unmounted_ui_handle)
+        })
+    }
+
+    fn pinned_render_update<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+        state: Pin<&mut experimental::PinnedStateOfKind<Renderer, Self::RenderStateKind>>,
+        ui_handle: &mut experimental::PinnedUiHandleOfKind<Renderer, Self::RenderStateKind>,
     ) {
         let Self {
             context_key,
             into_value,
             get_element,
         } = self;
+        let StateProj {
+            state,
+            context_key_and_value,
+        } = state.project();
         context_key_and_value.same_context_key_or_insert(context_key, into_value);
-        context_key_and_value.provide(|| {
-            get_element().pinned_render_update(
-                render_context,
-                RenderStates {
-                    ui_handle,
-                    non_reactive_state,
-                    reactive_state,
-                },
-            );
-        })
+        context_key_and_value
+            .provide(|| get_element().pinned_render_update(renderer, state, ui_handle))
     }
 
     fn unpinned_render_init<Ctx: ?Sized + frender_html::HtmlRenderContext>(
         //
         self,
         render_context: &mut Ctx,
-    ) -> experimental::UnpinnedRenderStatesOfKind<Self::RenderStateKind, Ctx::Renderer> {
+    ) -> (
+        //
+        experimental::UnpinnedStateOfKind<Ctx::Renderer, Self::RenderStateKind>,
+        experimental::UnpinnedUiHandleOfKind<Ctx::Renderer, Self::RenderStateKind>,
+    ) {
         let (mut context_key_and_value, get_element) = self.into_ctx_and_get_element();
 
-        let RenderStates {
-            ui_handle,
-            non_reactive_state,
-            reactive_state,
-        } = context_key_and_value.provide(|| get_element().unpinned_render_init(render_context));
+        let (state, ui_handle) =
+            context_key_and_value.provide(|| get_element().unpinned_render_init(render_context));
 
-        RenderStates {
-            ui_handle: UiHandleWithNonReactiveState {
-                ui_handle,
-                non_reactive_state: context_key_and_value,
+        (
+            State {
+                state,
+                context_key_and_value,
             },
-            non_reactive_state,
-            reactive_state,
-        }
+            ui_handle,
+        )
     }
 
-    fn unpinned_render_update<Ctx: ?Sized + frender_html::HtmlRenderContext>(
-        //
+    fn unpinned_render_init_by_reusing<Ctx: ?Sized + frender_html::HtmlRenderContext>(
         self,
         render_context: &mut Ctx,
-        RenderStates {
-            ui_handle:
-                UiHandleWithNonReactiveState {
-                    ui_handle,
-                    non_reactive_state: context_key_and_value,
-                },
-            non_reactive_state,
-            reactive_state,
-        }: experimental::UnpinnedMutRenderStatesOfKind<
-            Self::RenderStateKind,
+        reused_state: &mut experimental::UnpinnedStateOfKind<Ctx::Renderer, Self::RenderStateKind>,
+        unmounted_ui_handle: experimental::UnpinnedUnmountedUiHandleOfKind<
             Ctx::Renderer,
+            Self::RenderStateKind,
         >,
+    ) -> experimental::UnpinnedUiHandleOfKind<Ctx::Renderer, Self::RenderStateKind> {
+        let Self {
+            context_key,
+            into_value,
+            get_element,
+        } = self;
+        let State {
+            state,
+            context_key_and_value,
+        } = reused_state;
+        context_key_and_value.same_context_key_or_insert(context_key, into_value);
+        context_key_and_value.provide(|| {
+            get_element().unpinned_render_init_by_reusing(
+                render_context,
+                state,
+                unmounted_ui_handle,
+            )
+        })
+    }
+
+    fn unpinned_render_update<Renderer: ?Sized + RenderHtml>(
+        //
+        self,
+        renderer: &mut Renderer,
+        State {
+            state,
+            context_key_and_value,
+        }: &mut experimental::UnpinnedStateOfKind<Renderer, Self::RenderStateKind>,
+        ui_handle: &mut experimental::UnpinnedUiHandleOfKind<Renderer, Self::RenderStateKind>,
     ) {
         let Self {
             context_key,
@@ -243,15 +288,7 @@ impl<
         } = self;
         context_key_and_value.same_context_key_or_insert(context_key, into_value);
 
-        context_key_and_value.provide(|| {
-            get_element().unpinned_render_update(
-                render_context,
-                RenderStates {
-                    ui_handle,
-                    non_reactive_state,
-                    reactive_state,
-                },
-            );
-        })
+        context_key_and_value
+            .provide(|| get_element().unpinned_render_update(renderer, state, ui_handle))
     }
 }
